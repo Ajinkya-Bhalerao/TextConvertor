acb7db0dc0321d07bb8a8a87493eca3c9f848b74
Ajinkya <ajinkya.bhalerao@csu.fullerton.edu>
2023-01-13 18:31:42 -0800

    learning props

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import PropTypes from "prop-types";
+export default function NavigationBar(props) {
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/">{props.about}</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+// handling props efficiently this will help to identify error if props are not passed and to set the default values
+NavigationBar.propTypes = { title: PropTypes.string.isRequired, about: PropTypes.string.isRequired};
+
+// We have not passed About prop from APP.js therefor it will take default value of about=About and display.
+NavigationBar.defaultProps = { title: "Set Title", about: "About" };
